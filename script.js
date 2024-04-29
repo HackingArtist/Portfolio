7a5dd65a6d826922b1ae0605414c1cac74b82050
Aashish <aashishmethani5@gmail.com>
2024-04-29 21:17:40 +0530

    Major Updates addign FD Casestudy

--- script.js
+++ script.js
@@ -124,6 +124,18 @@ function() {
 //     }
 
 
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
 
 
